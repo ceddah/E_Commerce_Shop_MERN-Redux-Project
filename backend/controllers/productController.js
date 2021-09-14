480fd54fc42b132f2e99156bc114843489cf3d2f
ceddah <cedanik1@gmail.com>
2021-09-14 13:40:57 +0200

    getSingleProduct route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -15,3 +15,20 @@ exports.newProduct = async (req, res, next) => {
         product: product
      })
 }
+
+// => /api/v1/product/:id
+exports.getSingleProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+
+    if(!product) {
+        return res.status(404).json({ 
+            success: false,
+            message: 'Product not Found.' 
+        })
+    }
+
+    res.status(200).json({
+        success: true,
+        product: product
+    });
+}
