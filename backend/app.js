7c4d58aa3a471f30021910f43cfc5559f6d9a065
ceddah <cedanik1@gmail.com>
2021-09-16 14:08:01 +0200

    ...

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const cookieParser = require('cookie-parser')
 const errorMiddleware = require('./middlewares/errors');
 //Routes
 const products = require('./routes/product');
 const auth = require('./routes/auth');
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 
 app.use('/api/v1', products);
