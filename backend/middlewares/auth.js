f76042a5cab4f732dbce190fcdc058a7abf78a2a
ceddah <cedanik1@gmail.com>
2021-09-16 23:37:35 +0200

    Sending Cookies, isAuth middleware, check user.role middleware

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -8,7 +8,7 @@ exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
     const { token } = req.cookies;
 
     if(!token) {
-        return next(new ErrorHandler('Login first to access this resource', 401));
+        return next(new ErrorHandler('Login first to access this resource.', 401));
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -16,3 +16,13 @@ exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
 
     next();
 });
+
+// Handling User Roles
+exports.authorizeRoles = (...roles) => {
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role)) {
+            return next(new ErrorHandler(`Role (${req.user.role}) is not allowed to access this resouce.`, 403))
+        };
+        next();
+    }
+};
