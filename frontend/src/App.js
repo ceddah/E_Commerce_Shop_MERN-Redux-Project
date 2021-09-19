db7effd9eb81264bb54d8f916180ea672d5f8d80
ceddah <cedanik1@gmail.com>
2021-09-19 20:23:51 +0200

    Display Single Product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import Home from './components/Home';
+import ProductDetails from './components/product/ProductDetails'
 
 import * as ROUTES from './constants/routes';
 
@@ -15,6 +16,7 @@ function App() {
         <Header />
         <div className="container container-fluid">
           <Route path={ROUTES.HOME} component={Home} exact />
+          <Route path={ROUTES.PRODUCT_DETAILS} component={ProductDetails} exact />
         </div>
         <Footer />
       </div>
